31cac1fc7870d1fcfd9124585748de26aa37a725
Suphi GEZGİN <s.suphi.gezgin@gmail.com>
2022-04-14 12:41:51 +0300

    ...

---
+++ TaekReport/BasvuruFormu/rprBasvuruFormu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace TaekReport.BasvuruFormu
+{
+    public partial class rprBasvuruFormu : DevExpress.XtraReports.UI.XtraReport
+    {
+        public rprBasvuruFormu()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
