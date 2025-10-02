f3171df0a97c35a6b42d54e0628490e4de50c3df
s.suphi.gezgin@gmail.com <s.suphi.gezgin@gmail.com>
2025-10-02 11:26:24 +0300

    ..

--- TaekReport/KAEK/ReportBase.cs
+++ TaekReport/KAEK/ReportBase.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaekReport.KAEK.Models;
 
 namespace TaekReport.KAEK
 {
-    class ReportBase
+    public class ReportBase
     {
+        public byte[] UstBasvuruFrm(UstBasvuruModel form)
+        {
+            byte[] resultReport = null;
+            try
+            {
+                KAEK.UstBasvuru.frmUstBasvuru rpr = new KAEK.UstBasvuru.frmUstBasvuru(form);                
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    rpr.ExportToPdf(ms);
+                    resultReport = ms.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return resultReport;
+        }
     }
 }
