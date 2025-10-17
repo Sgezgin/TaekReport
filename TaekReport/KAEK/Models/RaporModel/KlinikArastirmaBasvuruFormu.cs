0d622a95461781437034fb54d75bb07aa67d329e
çağrı özen <cagriozen58@gmail.com>
2025-10-17 15:50:43 +0300

    KLINIKARASTIRMALARBASVURUFORMU

---
+++ TaekReport/KAEK/Models/RaporModel/KlinikArastirmaBasvuruFormu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaekReport.KAEK.Models.RaporModel
+{
+    public class KlinikArastirmaBasvuruFormu
+    {
+        public KLINIKARASTIRMALARBASVURUFORMU KlinkFormu { get; set; }
+
+    }
+}
+
+
