433363c87c1a8f485cb750d989529e4cb8735074
çağrı özen <cagriozen58@gmail.com>
2025-11-25 15:55:21 +0300

    ...

--- TaekReport/KAEK/IlacArastirma/rprKlinikArastirmaBasvuruFormu.cs
+++ TaekReport/KAEK/IlacArastirma/rprKlinikArastirmaBasvuruFormu.cs
@@ -3,14 +3,16 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using DevExpress.XtraReports.UI;
+using TaekReport.KAEK.Models.RaporModel;
 
 namespace TaekReport.KAEK.IlacArastirma
 {
     public partial class rprKlinikArastirmaBasvuruFormu : DevExpress.XtraReports.UI.XtraReport
     {
-        public rprKlinikArastirmaBasvuruFormu()
+        public rprKlinikArastirmaBasvuruFormu(KlinikArastirmaBasvuruFormu  o)
         {
             InitializeComponent();
+            objectDataSource1.DataSource = o;
         }
 
     }
